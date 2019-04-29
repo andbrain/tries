2e38e6a177a190487e409311ee2da7ae84808313
Anderson Pimentel <andbrain@gmail.com>
2019-04-29 14:17:25 -0400

    toy dataset american-names

--- main.cpp
+++ main.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
+#include <vector>
 #include "include/trie.h"
+#include <fstream>
+
+
+std::vector<std::string> data;
+void readData(std::string filename);
 
 int main(int argc, char const *argv[]) {
-
-    std::string vocabulary[] = {
-            "hello",
-            "dog",
-            "hell",
-            "cat",
-            "a",
-            "hel",
-            "help",
-            "helps",
-            "helping"
-    };
-
-//    int n = sizeof(vocabulary)/ sizeof(vocabulary[0]);
+    readData("/home/anderson/autocompletion/test/american-names.txt");
 
     SimpleTrie2::trie t1;
 
-    for (auto voc: vocabulary){
+    for (auto voc: data){
         std::cout << "inserting: " << voc << std::endl;
         t1.insert(voc);
     }
@@ -27,14 +20,18 @@ int main(int argc, char const *argv[]) {
     std::cout << std::endl;
 
     std::cout << "[Words indexed] -> ok" << std::endl;
-    std::cout << std::endl;
-    std::cout << std::endl;
     std::cout << "[Tasks with trie]" << std::endl;
 
-    std::cout << "searching: hel -> " << t1.search("hel") << std::endl;
-    std::cout << "searching: he -> " << t1.search("he") << std::endl;
-    std::cout << "Autocompletion of 'hel'" << std::endl;
-
-    t1.autocomplete("hel");
+    std::cout << "searching: Jeff -> " << t1.search("Jeff") << std::endl;
+    std::cout << "Autocompletion of 'J'" << std::endl;
+    t1.autocomplete("Jo");
     return 0;
+}
+
+void readData(std::string filename){
+    std::ifstream infile(filename);
+    std::string line;
+
+    while(std::getline(infile,line))
+        data.push_back(line);
 }
